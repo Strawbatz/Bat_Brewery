8ae1a430acf39d6c676181b9f0254e231aed2d6f
Samuel Dahlborg <beaosam@gmail.com>
2024-03-07 15:29:22 +0100

    UI tweaks

--- Bat_Brewery/Assets/Item System/TagMenuButton.cs
+++ Bat_Brewery/Assets/Item System/TagMenuButton.cs
@@ -16,12 +16,17 @@ public class TagMenuButton : MonoBehaviour, ISelectHandler, IDeselectHandler
 
     [SerializeField] private Color baseColor;
     [SerializeField] private Color selectedColor;
+    private bool lastSelected = false;
     void Start()
     {
         thisButton = GetComponent<Button>();
         thisButton.onClick.AddListener(Clicked);
         GetComponentsInChildren<Image>()[1].sprite = visualTag.GetWorldImg();
         transform.localScale = Vector3.one;
+    }
+
+    public void SetLastSelected() {
+        lastSelected = true;
     }
 
     private void Clicked() {
@@ -34,13 +39,19 @@ public class TagMenuButton : MonoBehaviour, ISelectHandler, IDeselectHandler
     }
 
     public void OnDeselect(BaseEventData eventData){
-        GetComponentsInChildren<Image>()[0].color = baseColor;
-        GetComponentsInChildren<Image>()[2].color = new Color(1,1,1);
+        if(lastSelected) {
+            GetComponentsInChildren<Image>()[0].color = new Color(0.5f,0.5f,0.5f);
+            GetComponentsInChildren<Image>()[2].color = new Color(0.5f,0.5f,0.5f);
+        } else {
+            GetComponentsInChildren<Image>()[0].color = baseColor;
+            GetComponentsInChildren<Image>()[2].color = new Color(1,1,1);
+        }
     }
 
     private void OnDisable() {
         GetComponentsInChildren<Image>()[0].color = baseColor;
         GetComponentsInChildren<Image>()[2].color = new Color(1,1,1);
+        lastSelected = false;
     }
 
 }
